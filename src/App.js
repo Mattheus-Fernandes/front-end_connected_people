be9861300cb6a42e8778676e699d590bd93cf465
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-02 13:48:10 -0300

    feat: checking authentication

--- src/App.js
+++ src/App.js
@@ -6,18 +6,31 @@ import Navbar from './component/Navbar/Navbar';
 //Pages
 import Login from "./pages/Login/Login"
 import Register from "./pages/Register/Register"
+import Home from './pages/Home/Home';
 
 //Router
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+//Hook
+import { useAuth } from './hooks/useAuth';
+
 
 function App() {
+
+  const {auth, loading} = useAuth()
+  
+  if(loading){
+    return <h1>Carregando...</h1>
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route path='/' element={Home}/>
+          <Route path='/login' element={!auth ? <Login /> : <Navigate to="/" />} />
+          <Route path='/register' element={!auth ? <Register /> : <Navigate to="/"/>} />
         </Routes>
       </BrowserRouter>
     </div>
