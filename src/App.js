648280201ab22038d1c822eb5f00af6dc977e1b9
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-02 13:40:56 -0300

    import and renderization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+//Components
+import Navbar from './component/Navbar/Navbar';
+
+//Pages
+import Login from "./pages/Login/Login"
+import Register from "./pages/Register/Register"
+
+//Router
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
