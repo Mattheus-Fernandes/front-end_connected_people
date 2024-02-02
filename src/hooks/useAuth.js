f1f89e72dcf30f8df0431cc8cf625ec8df282580
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-02 14:15:57 -0300

    feat: create hook of authentication

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+
+//Verificar autenticação
+export const useAuth = () => {
+
+
+  const {user} = useSelector(state => state.auth)
+
+  const [auth, setAuth] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+
+    if(user){
+      setAuth(true)
+    }else {
+      setAuth(false)
+    }
+
+    setLoading(false)
+    
+
+  }, [user])
+
+
+  return {auth, loading}
+
+
+}
