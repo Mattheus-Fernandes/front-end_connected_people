21e9eaa85051d04cc6817f77cc02f76ca219822a
Mattheus Fernandes <mattheus.ale@hotmail.com>
2024-02-02 14:37:18 -0300

    feat: create service of login

--- src/services/authService.js
+++ src/services/authService.js
@@ -33,9 +33,35 @@ const logout = () => {
 }
 
 
+
+const login = async(data) => {
+
+  const config = requestConfig("POST", data)
+
+  try {
+
+    const res = await fetch(api + "/login", config)
+                      .then(res => res.json())
+                      .catch(err => err)
+    if(res.id){
+
+      localStorage.setItem("user", JSON.stringify(res))
+
+    }
+
+
+    return res
+  }catch(error){
+    console.log(error)
+  }
+
+}
+
+
 const authService = {
   register,
-  logout
+  logout,
+  login
 }
 
 export default authService
